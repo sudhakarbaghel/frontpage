fddf1e37a295d9f30e16756713c997907836879f
your name <sudhakarsinghbaghel123321@gmail.com>
2023-02-01 19:24:01 +0530

    coommit

--- src/components/domains/Domains.jsx
+++ src/components/domains/Domains.jsx
@@ -1,4 +1,4 @@
-import styles from "./domains.module.css"
+import  "./domains.scss"
 import video from "../../assets/camera.svg"
 import graphic from "../../assets/graphic.svg"
 import digital from "../../assets/digital.svg"
@@ -10,48 +10,48 @@ import data from "../../assets/data.svg"
 
 export default function Domains() {
   return (
-      <div className={styles.domain}>
-          <div className={styles.wrap}>
-              <span className={styles.heading}>Explore Domains</span>
-              <hr className={styles.rule} />
+      <div className="domain">
+          <div className="domainWrap">
+              <span className="domainHeading">Explore Domains</span>
+              <hr className="domainRule" />
           </div>
          
-          <div className={styles.iconContainer}>
-              <div className={styles.iconWrap}>
+          <div className="domainIconContainer">
+              <div className="domainIconWrap">
                   <img src={video} alt="" />
                   <span>Video and Animation</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={graphic} alt="" />
                   
                   <span>Graphic Design</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={digital} alt="" />
                   
                   <span>Digital Marketing</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={writing} alt="" />
                   
                   <span>Writing and Translation</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={music} alt="" />
                   
                   <span>Music and Audio</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={buisness} alt="" />
                   
                   <span>Business</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={programming} alt="" />
                   
                   <span>Programming and Tech</span>
               </div>
-              <div className={styles.iconWrap}>
+              <div className="domainIconWrap">
                   <img src={data} alt="" />
                   
                   <span>Data Analysis</span>
